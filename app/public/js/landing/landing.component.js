3676a320a569aceee9c5a0f3636a225ae5283eac
Devin Hurd <hurdaudio@gmail.com>
2018-05-31 20:55:04 -0600

    fixes to landing page

--- app/public/js/landing/landing.component.js
+++ app/public/js/landing/landing.component.js
@@ -75,7 +75,7 @@
 
       function onInit() {
         console.log("Landing is lit");
-        let landingPic = document.getElementById('landingPic');
+        let landingPic = document.getElementById('contentImg');
         landingPic.setAttribute("style", "opacity: 0.2; filter: hue-rotate(60deg);");
         setTimeout(()=>{
           landingPic.setAttribute("style", "opacity: 1; filter: hue-rotate(0deg); transition: all 1s linear;");
