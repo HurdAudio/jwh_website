3768cc5b0b944486f29e843a6638f3c9cdbb80e0
Devin Hurd <hurdaudio@gmail.com>
2018-06-03 18:20:49 -0600

    about memoir page edits

--- app/public/js/memoir/memoir.component.js
+++ app/public/js/memoir/memoir.component.js
@@ -1,6 +1,13 @@
 (function() {
   'use strict';
 
+  var memoirDropdownToggle = false;
+  var funStuffDropdownToggle = false;
+  var overMemoirMenu = false;
+  var overAuthorMenu = false;
+  var overFunStuffMenu = false;
+  var authorDropdownToggle = false;
+
 
   angular.module('app')
     .component('memoir', {
@@ -15,20 +22,141 @@
 
       vm.$onInit = onInit;
       vm.navigateNovels = navigateNovels;
-      vm.navigateLanding = navigateLanding;
-      vm.navigateToSurprise = navigateToSurprise;
+      vm.navigateMemoir = navigateMemoir;
       vm.navigateShortStories = navigateShortStories;
+      vm.surpisedbystory = surpisedbystory;
+      vm.hoverOffMemoir = hoverOffMemoir;
+      vm.memoirMenuActive = memoirMenuActive;
+      vm.memoirMenueInactive = memoirMenueInactive;
+      vm.authorMenuActive = authorMenuActive;
+      vm.authorMenueInactive = authorMenueInactive;
+      vm.hoverOffAuthor = hoverOffAuthor;
+      vm.navigateAuthor = navigateAuthor;
+      vm.funStuffMenuActive = funStuffMenuActive;
+      vm.funStuffMenuInactive = funStuffMenuInactive;
+      vm.hoverOffFunStuff = hoverOffFunStuff;
+      vm.navigateFunStuff = navigateFunStuff;
+      vm.navigateContact = navigateContact;
+      vm.navigateWelome = navigateWelome;
+
+      function navigateWelome() {
+        $state.go('landing');
+      }
+
+      function navigateContact() {
+        $state.go('contact');
+      }
+
+      function funStuffMenuInactive() {
+        overFunStuffMenu = false;
+        hoverOffFunStuff();
+      }
+
+      function authorMenueInactive() {
+        overAuthorMenu = false;
+        hoverOffAuthor();
+      }
+
+      function funStuffMenuActive() {
+        overFunStuffMenu = true;
+      }
+
+      function authorMenuActive() {
+        overAuthorMenu = true;
+      }
+
+      function memoirMenuActive() {
+        overMemoirMenu = true;
+      }
+
+      function memoirMenueInactive() {
+        overMemoirMenu = false;
+        hoverOffMemoir();
+      }
+
+
+      function surpisedbystory() {
+        $state.go('memoir');
+      }
 
       function navigateShortStories() {
         $state.go('shortstories');
       }
 
-      function navigateToSurprise() {
-        $state.go('surprisedbystory');
+      function navigateFunStuff() {
+        let funStuffDropdownDiv = document.getElementById('funStuffDropdownDiv');
+
+        if (funStuffDropdownToggle) {
+          funStuffDropdownToggle = false;
+          funStuffDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+        } else {
+          funStuffDropdownToggle = true;
+          funStuffDropdownDiv.setAttribute("style", "visibility: visible; opacity: 1; transition: opacity 0.3s linear; z-index: 10;");
+        }
       }
 
-      function navigateLanding() {
-        $state.go('landing');
+      function navigateAuthor() {
+        let authorDropdownDiv = document.getElementById('authorDropdownDiv');
+
+        if (authorDropdownToggle) {
+          authorDropdownToggle = false;
+          authorDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+        } else {
+          authorDropdownToggle = true;
+          authorDropdownDiv.setAttribute("style", "visibility: visible; opacity: 1; transition: opacity 0.3s linear; z-index: 10;");
+        }
+      }
+
+      function navigateMemoir() {
+        let memoirDropdownDiv = document.getElementById('memoirDropdownDiv');
+
+        if (memoirDropdownToggle) {
+          memoirDropdownToggle = false;
+          memoirDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+        } else {
+          memoirDropdownToggle = true;
+          memoirDropdownDiv.setAttribute("style", "visibility: visible; opacity: 1; transition: opacity 0.3s linear; z-index: 10;");
+        }
+        // $state.go('memoir');
+      }
+
+      function hoverOffFunStuff() {
+        let funStuffDropdownDiv = document.getElementById('funStuffDropdownDiv');
+
+        if (funStuffDropdownToggle && !overFunStuffMenu) {
+          setTimeout(()=>{
+            if (funStuffDropdownToggle && !overFunStuffMenu) {
+              funStuffDropdownToggle = false;
+              funStuffDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+            }
+          }, 500);
+        }
+      }
+
+      function hoverOffAuthor() {
+        let authorDropdownDiv = document.getElementById('authorDropdownDiv');
+
+        if (authorDropdownToggle && !overAuthorMenu) {
+          setTimeout(()=>{
+            if (authorDropdownToggle && !overAuthorMenu) {
+              authorDropdownToggle = false;
+              authorDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+            }
+          }, 500);
+        }
+      }
+
+      function hoverOffMemoir() {
+        let memoirDropdownDiv = document.getElementById('memoirDropdownDiv');
+
+        if (memoirDropdownToggle && !overMemoirMenu) {
+          setTimeout(()=>{
+            if (memoirDropdownToggle && !overMemoirMenu) {
+              memoirDropdownToggle = false;
+              memoirDropdownDiv.setAttribute("style", "visibility: hidden; opacity: 0; transition: opacity 0.3s linear; z-index: -10;");
+            }
+          }, 500);
+        }
       }
 
       function navigateNovels() {
